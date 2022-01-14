30d4c5e59776e5bacfdcd10368c27bc120fd64dd
lupudeni <54315826+lupudeni@users.noreply.github.com>
2022-01-14 14:42:06 +0200

    populate database

--- src/main/java/sia/tacocloud/util/Path.java
+++ src/main/java/sia/tacocloud/util/Path.java
@@ -2,7 +2,7 @@ package sia.tacocloud.util;
 
 public class Path {
 
-    public static final String REDIRECT = "/redirect";
+    public static final String REDIRECT = "redirect:/";
 
     public static final String HOME = "home";
 }
