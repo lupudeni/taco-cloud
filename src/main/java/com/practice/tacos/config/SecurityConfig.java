a5192bfc1c30a2560a891011590159a6cd8a6a2c
lupudeni <54315826+lupudeni@users.noreply.github.com>
2025-02-10 12:45:39 +0200

    add login

--- src/main/java/com/practice/tacos/config/SecurityConfig.java
+++ src/main/java/com/practice/tacos/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.practice.tacos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,4 +15,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                    .requestMatchers("/design", "/orders").hasRole("USER")
+                    .requestMatchers("/", "/**").permitAll()
+                )
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+                        .loginPage("/login") //This is default anyway
+                        .defaultSuccessUrl("/design"))
+                .build();
+
+    }
 }
